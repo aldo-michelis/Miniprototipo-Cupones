abc4247c648a32de712ea0e667646893e8b41f53
Emmanuel Frias <emfrigo@hotmail.com>
2018-08-31 21:01:13 -0500

    Se agrega la pagina principal de negocio

--- resources/views/layouts/totales.blade.php
+++ resources/views/layouts/totales.blade.php
@@ -1,18 +1,11 @@
-@if ( Auth::user()->user_type == 1 )
-    <label for="">Promos Entregadas: </label>
-    <br>
-    <label for="">$ {{ Auth::user()->totalDePromociones() }} </label>
-    <br>
-    <label for="">Saldo en MC: </label>
-    <br>
-    <label for="">$ {{ Auth::user()->mc_saldo }} </label>
-    <br>
-@else
-    <section class="edoCuenta">
-        Monedas Electrónicas
-            <strong>${{ number_format( Auth::user()->mc_saldo, 2, '.', ',') }}</strong>
-        <br>
+<section class="edoCuenta">
+Monedas Electrónicas
+<strong>${{ number_format( Auth::user()->mc_saldo, 2, '.', ',') }}</strong>
+<br>
+    @if ( Auth::user()->user_type == 1 )
+        Promociones entregadas
+    @else
         Descuentos acumulados
-            <strong>${{ number_format( Auth::user()->totalDePromociones(), 2, '.', ',') }}</strong>
-    </section>
-@endif
+    @endif
+    <strong>${{ number_format( Auth::user()->totalDePromociones(), 2, '.', ',') }}</strong>
+</section>
