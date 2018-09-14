e41a85aa19eef349dd604279d4d5b85032490ef7
Emmanuel Frias <emfrigo@hotmail.com>
2018-09-14 08:48:11 -0500

    Se configura la descarga del codigo qr

--- resources/views/merchants/promocion.blade.php
+++ resources/views/merchants/promocion.blade.php
@@ -8,12 +8,12 @@
 <main>
     <h3>Enlace de Promoción</h3>
     <strong>
-        <a href="http://cupones.com/promocion/{{ $enlace }}" target="_blank">
+        <a href="http://18.223.232.129/public/promocion/{{ $enlace }}" target="_blank">
             http://18.223.232.129/public/promocion/{{ $enlace }}
         </a>
     </strong>
     <br>
-    <a href="http://192.168.0.13/Miniprototipo-Cupones/public/code/{{ $id }}">Descargar Codigo QR</a>
+    <a href="http://18.223.232.129/public/code/{{ $id }}">Descargar Codigo QR</a>
     <br>
     {!! QrCode::size(200)->margin(3)->generate('http://18.223.232.129/public/promocion/'.$enlace) !!}
 </main>
