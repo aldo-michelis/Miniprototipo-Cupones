bbba7307329d0184634777a6b86402aa704feeb6
Emmanuel Frias <emfrigo@hotmail.com>
2018-09-14 11:20:52 -0500

    Se repara la seleccion y envio de correos

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -225,7 +225,9 @@ class CustomerController extends Controller
 
     public function enviarPorMensaje($id)
     {
-        $detail = CouponDetail::where('id', $id)->get();
+        $detail = CouponDetail::where('coupon_id', $id)
+                                ->where('user_id', auth()->id)
+                                ->where('status', 0)->get();
         Mail::to(auth()->user()->username)->send(new CodeSendingMail($detail[0]));
         return response()->json(['status' => true]);
     }
